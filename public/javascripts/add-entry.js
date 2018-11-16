c35e52095b6e2551c56c61a1fc43d1d887e8b40f
kjiang12 <7127959+kjiang12@users.noreply.github.com>
2018-11-15 22:35:45 -0500

    Added Idea Submission

---
+++ public/javascripts/add-entry.js
@@ -0,0 +1,16 @@
+function handleFileSelect(e){
+    let file = e.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = function(){
+        let img = document.getElementsByTagName('img')[0];
+        if(img == null){
+            img = document.createElement('img');
+            document.getElementsByClassName("dropZoneContainer")[0].prepend(img);
+            $('.dropZoneOverlay').removeClass('empty');
+            $('.dropZoneOverlay').addClass('filled');
+        }
+        img.src = reader.result;
+    };
+
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET add idea page. */
+router.get('/add-entry', function(req, res, next) {
+    res.render('add-entry', { title: 'Submit a New Idea' });
+});
+
+/* POST add idea page. */
+router.post('/add-entry', upload.single('pic'), function (req, res, next) {
+    let image = req.file;
+    let title = req.body.title;
+    let description = req.body.description;
+    let tags = req.body.tags;
+
+    //TODO Add to DB here
+
+    res.render('add-entry-success', { title: 'Submission Success' });
 });
 
 module.exports = router;
